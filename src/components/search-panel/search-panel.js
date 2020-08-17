21ec2bf22b842af70d37872d28be90b16c1f0bb2
Marie Colesnic <melterina96@gmail.com>
2020-08-17 14:44:24 +0300

    Added case-insensitive search realisation

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, {Component} from "react";
 import './search-panel.css';
 
-const SearchPanel = () => {
+
+export default class SeacrhPanel extends Component {
+
+state = {
+  term: ''
+}
+
+onSearchChange = (e) => {
+  const term = e.target.value;
+  this.setState({term});
+  this.props.onSearchChange(term);
+}
+
+
+  render () {
     return (
-      <input type="text"
-                className="form-control search-input"
-                placeholder="type to search" />
+    <input type="text"
+              className="form-control search-input"
+              placeholder="type to search" 
+              value={this.state.term} 
+              onChange={this.onSearchChange}/>
     );
-  };
-  
-export default SearchPanel;
+  }
+}
+
+
+const SearchPanel = () => {
+    
+}
